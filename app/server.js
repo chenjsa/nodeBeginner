a1b7197ca8e4f8c33a4e3a68cdec59bf8e1d9ce9
陈俊生 <junsheng.chen@ggcar.com>
2016-12-19 09:25:16 +0800

    Added .babelrc

--- app/server.js
+++ app/server.js
@@ -8,12 +8,10 @@ import http from 'http';
 import url from 'url';
 
 export function start(route, handle) {
-    function onRequest(request, response) {
+    http.createServer((request, response) => {
         let pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
         route(handle, pathname, response, request);
-    }
-
-    http.createServer(onRequest).listen(8888);
+    }).listen(8888);
     console.log("Server has started.");
 }
